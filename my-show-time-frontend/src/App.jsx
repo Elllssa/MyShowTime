e22938df7fccf8daafc4bef487c46ab34d3f5c02
Elllssa <Elllssa>
2023-12-13 11:55:26 +0100

    add front

---
+++ my-show-time-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p className="underline">Test</p>
+    </div>
+  )
+}
+
+export default App
---
+++ my-show-time-frontend/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "Jade1": "#FBFEFD",
+        "Jade2": "#F4FBF7",
+        "Jade3": "#E6F7ED",
+        "Jade4": "#D6F1E3",
+        "Jade5": "#C3E9D7",
+        "Jade6": "#ACDEC8",
+        "Jade7": "#8BCEB6",
+        "Jade8": "#56BA9F",
+        "Jade9": "#29A383",
+        "Jade10": "#26997B",
+        "Jade11": "#208368",
+        "Jade12": "#1D3B31",
+      },
+    },
+  },
+  plugins: [],
+}
+
